416f8a54325ef2172654909eb2023f3c17b8e778
Ezequiel Sosa <ezequieljsosa@gmail.com>
2022-01-26 13:57:59 -0300

    app change

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ function App() {
   console.log(process.env.REACT_APP_ALGO);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>una app: {process.env.REACT_APP_ALGO} </h1>
     </div>
   );
 }
