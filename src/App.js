55c2d6187c1d87a509965e2a9eddcfe3ecbb37ee
Ezequiel Sosa <ezequieljsosa@gmail.com>
2022-01-26 14:49:25 -0300

    fetch agregado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log(process.env.REACT_APP_ALGO);
-  return (
-    <div className="App">
-     <h1>una app: {process.env.REACT_APP_ALGO} </h1>
-    </div>
-  );
+    const [result, setResult] = useState("...");
+    const buscar = () => {
+        fetch(process.env.REACT_APP_ALGO || "localhost:8080/hola").then(request => request.text()).then(data => setResult(data));
+    };
+    return (
+        <div className="App">
+            <h1>una app: {process.env.REACT_APP_ALGO} </h1>
+            <button onClick={buscar}>Buscar al server</button>
+        </div>
+    );
 }
 
 export default App;
