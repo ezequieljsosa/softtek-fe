9941bc88b5fda070db981f43418f570d39eff36d
Ezequiel <ezequieljsosa@gmail.com>
2022-01-26 18:55:14 -0300

    para deploy

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 function App() {
     const [result, setResult] = useState("...");
     const buscar = () => {
-        fetch(process.env.REACT_APP_BACK_ENDPOINT || "http://localhost:8080/hola").then(request => request.text()).then(data => setResult(data));
+        fetch(process.env.REACT_APP_BACK_ENDPOINT || "http://localhost:8080/hola").then(
+            request => request.text()).then(data => setResult(data));
     };
     return (
         <div className="App">
             <h1>una app: {process.env.REACT_APP_ALGO} </h1>
             <button onClick={buscar}>Buscar al server</button>
-            <h1> Result: </h1>
+            <h1> Result2: </h1>
             <h2>{result}</h2>
         </div>
     );
